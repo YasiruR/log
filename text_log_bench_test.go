3a555c49c39a0a68a60959b2098e17ef6d18df1b
Kosala Tennakoon <kosala.me@gmail.com>
2021-06-08 13:54:03 +0530

    updated bench tests

--- text_log_bench_test.go
+++ text_log_bench_test.go
@@ -3,51 +3,182 @@ package log_test
 import (
 	"context"
 	"io/ioutil"
-	"log"
+	nativeLog "log"
 	"testing"
 
-	"github.com/google/uuid"
-	lg "github.com/tryfix/log"
-	traceable_context "github.com/tryfix/traceable-context"
+	"github.com/tryfix/log"
 )
 
-var txtLg = lg.NewLog(lg.WithLevel(lg.INFO), lg.WithStdOut(ioutil.Discard), lg.WithFilePath(true), lg.WithColors(true))
-var txtLogger = txtLg.Log()
-var native = log.New(ioutil.Discard, `test`, log.Lmicroseconds)
+const testLog = `test log entry, test log entry, test log entry, test log entry, test log entry`
 
-var txtTestCtx = traceable_context.WithUUID(uuid.New())
-
-func BenchmarkTextNative(b *testing.B) {
+func BenchmarkGoNative(b *testing.B) {
+	lg := nativeLog.New(ioutil.Discard, `test-logger.path`, nativeLog.Lmicroseconds)
+	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			native.Println(`dd`)
+			lg.Println(testLog)
 		}
 	})
 }
 
-func BenchmarkTextInfo(b *testing.B) {
+func BenchmarkTextLogInfo(b *testing.B) {
+	lg := log.NewLog(
+		log.WithLevel(log.INFO),
+		log.WithStdOut(ioutil.Discard),
+		log.WithFilePath(false),
+		log.WithColors(false)).Log()
+	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			txtLogger.Info(`dd`)
+			lg.Info(testLog)
+		}
+	})
+}
+
+func BenchmarkTextLogInfoFilePath(b *testing.B) {
+	lg := log.NewLog(
+		log.WithLevel(log.INFO),
+		log.WithStdOut(ioutil.Discard),
+		log.WithFilePath(true),
+		log.WithColors(false)).Log()
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			lg.Info(testLog)
 		}
 	})
 }
 
 func BenchmarkTextInfoContext(b *testing.B) {
+	lg := log.NewLog(
+		log.WithLevel(log.INFO),
+		log.WithStdOut(ioutil.Discard),
+		log.WithFilePath(false),
+		log.WithColors(false)).Log()
+	ctx := context.Background()
+	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			txtLogger.InfoContext(txtTestCtx, `dd`)
+			lg.InfoContext(ctx, testLog)
+		}
+	})
+}
+
+func BenchmarkTextInfoContextExt(b *testing.B) {
+	ctx1 := context.WithValue(context.Background(), `ctx1`, `ctx one value`)
+	ctx2 := context.WithValue(ctx1, `ctx2`, `ctx two value`)
+	lg := log.NewLog(
+		log.WithLevel(log.INFO),
+		log.WithStdOut(ioutil.Discard),
+		log.WithFilePath(false),
+		log.WithCtxExtractor(func(ctx context.Context) []interface{} {
+			return []interface{}{ctx.Value(`ctx1`), ctx.Value(`ctx2`)}
+		}),
+		log.WithColors(false)).Log()
+
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			lg.InfoContext(ctx2, testLog)
 		}
 	})
 }
 
 func BenchmarkTextInfoParams(b *testing.B) {
+	lg := log.NewLog(
+		log.WithLevel(log.INFO),
+		log.WithStdOut(ioutil.Discard),
+		log.WithFilePath(false),
+		log.WithColors(false)).Log()
+	ctx := context.Background()
+	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			other := txtLogger.NewLog(lg.Prefixed(`ss`))
-			go other.InfoContext(context.Background(), `dd`, 1, 2, 3)
-			xx := other.NewLog(lg.Prefixed(`ss`))
-			go xx.InfoContext(context.Background(), `dd`, 1, 2, 3)
+			lg.InfoContext(ctx, testLog,
+				`parm1`, `parm2`, `parm3`, `parm4`)
+		}
+	})
+}
+
+func BenchmarkJsonLogInfo(b *testing.B) {
+	lg := log.NewLog(
+		log.WithLevel(log.INFO),
+		log.WithOutput(log.OutJson),
+		log.WithStdOut(ioutil.Discard),
+		log.WithFilePath(false),
+		log.WithColors(false)).Log()
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			lg.Info(testLog)
+		}
+	})
+}
+
+func BenchmarkJsonLogInfoFilePath(b *testing.B) {
+	lg := log.NewLog(
+		log.WithLevel(log.INFO),
+		log.WithOutput(log.OutJson),
+		log.WithStdOut(ioutil.Discard),
+		log.WithFilePath(true),
+		log.WithColors(false)).Log()
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			lg.Info(testLog)
+		}
+	})
+}
+
+func BenchmarkJsonInfoContext(b *testing.B) {
+	lg := log.NewLog(
+		log.WithLevel(log.INFO),
+		log.WithOutput(log.OutJson),
+		log.WithStdOut(ioutil.Discard),
+		log.WithFilePath(false),
+		log.WithColors(false)).Log()
+	ctx := context.Background()
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			lg.InfoContext(ctx, testLog)
+		}
+	})
+}
+
+func BenchmarkJsonInfoContextExt(b *testing.B) {
+	ctx1 := context.WithValue(context.Background(), `ctx1`, `ctx one value`)
+	ctx2 := context.WithValue(ctx1, `ctx2`, `ctx two value`)
+	lg := log.NewLog(
+		log.WithLevel(log.INFO),
+		log.WithStdOut(ioutil.Discard),
+		log.WithFilePath(false),
+		log.WithOutput(log.OutJson),
+		log.WithCtxExtractor(func(ctx context.Context) []interface{} {
+			return []interface{}{ctx.Value(`ctx1`), ctx.Value(`ctx2`)}
+		}),
+		log.WithColors(false)).Log()
+
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			lg.InfoContext(ctx2, testLog)
+		}
+	})
+}
+
+func BenchmarkJsonInfoParams(b *testing.B) {
+	lg := log.NewLog(
+		log.WithLevel(log.INFO),
+		log.WithOutput(log.OutJson),
+		log.WithStdOut(ioutil.Discard),
+		log.WithFilePath(false),
+		log.WithColors(false)).Log()
+	ctx := context.Background()
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			lg.InfoContext(ctx, testLog, `parm1`, `parm2`, `parm3`, `parm4`)
 		}
 	})
 }
